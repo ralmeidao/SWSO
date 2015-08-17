4661e28252eebe9e6d4a96653d8778a1a0cad511
Ramon <Ramon@9009fd32-00b8-d34d-9bb3-231bff2beaf2>
2015-08-17 11:03:56 +0000

    Refactoring e Timeline de processos.

--- trunk/SWSO/src/main/java/br/edu/ifba/swso/controller/DiscoController.java
+++ trunk/SWSO/src/main/java/br/edu/ifba/swso/controller/DiscoController.java
@@ -2,24 +2,35 @@ package br.edu.ifba.swso.controller;
 
 import java.io.Serializable;
 
-import javax.faces.bean.RequestScoped;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.primefaces.model.UploadedFile;
+
+import br.edu.ifba.swso.algorithms.impl.disk.FCFS;
+import br.edu.ifba.swso.algorithms.impl.disk.SSTF;
+import br.edu.ifba.swso.algorithms.interfaces.IDiskScheduler;
 import br.edu.ifba.swso.business.VirtualMachineParameters;
 import br.edu.ifba.swso.business.abstractions.File;
+import br.edu.ifba.swso.business.abstractions.FileInput;
+import br.edu.ifba.swso.business.abstractions.Word;
+import br.edu.ifba.swso.business.so.OperatingSystem;
 import br.edu.ifba.swso.business.so.filemanager.IFileSystem;
 import br.edu.ifba.swso.business.virtualmachine.harddisk.HardDisk;
 import br.edu.ifba.swso.business.virtualmachine.harddisk.Plate;
 import br.edu.ifba.swso.business.virtualmachine.harddisk.Track;
 import br.edu.ifba.swso.display.MovimentoCabecoteHD;
+import br.edu.ifba.swso.util.Constantes;
+import br.edu.ifba.swso.util.Util;
 
 /**
  * @author Ramon
  */
 @Named
-@RequestScoped
-public class DiscoController implements Serializable {
+@SessionScoped
+public class DiscoController extends BaseController implements Serializable {
 
 	/**
 	 * 
@@ -29,6 +40,148 @@ public class DiscoController implements Serializable {
 	@Inject
 	private MaquinaSessaoController maquinaSessaoController;
 	
+	//BUSINESS DATA - START
+	private IDiskScheduler[] arrayDiskSchedule;
+	
+	private IDiskScheduler diskSchedule;
+	
+	private OperatingSystem operatingSystem;
+	
+	//VIEW DATA - START
+	private UploadedFile uploadFile;
+	
+	private String color;
+	
+	private String colorPersonalizar;
+	
+	private String movimentacaoSimulada;
+	
+	
+	@PostConstruct
+	public void init() {
+		VirtualMachineParameters virtualMachineParameters = maquinaSessaoController.getVirtualMachineParameters();
+		operatingSystem = maquinaSessaoController.getOperatingSystem();
+		arrayDiskSchedule = new IDiskScheduler[]{new SSTF(virtualMachineParameters), new FCFS(virtualMachineParameters)};
+		diskSchedule = arrayDiskSchedule[0];
+		operatingSystem.setDiskSchedule(diskSchedule);
+		arrayDiskSchedule = new IDiskScheduler[]{new SSTF(virtualMachineParameters), new FCFS(virtualMachineParameters)};
+	}
+	
+    public void salvarConfiguracoesDisco() {
+    	operatingSystem.setDiskSchedule(diskSchedule);
+    }
+    
+    public void doUploadFile() {
+    	if (validarUploadArquivo()) {
+    		FileInput fileInput = criarInputFile();
+    		operatingSystem.getFileSystem().allocateFile(fileInput, operatingSystem.getDiskSchedule());
+    		clearUpload();
+    		updateComponentes(":formSimulacao");
+    	}
+    }
+    
+    public void updateComboColor() {
+		updateComponentes(":uploadForm:selectColors");
+	}
+    
+    public void simularMovimentacao() {
+    	operatingSystem.getFileSystem().simularMovimentacao(movimentacaoSimulada, operatingSystem.getDiskSchedule());
+    }
+    
+    public void deleteFile(Integer id) {
+    	operatingSystem.getFileSystem().deallocateFile(id);
+    }
+    
+	private FileInput criarInputFile() {
+		byte[] arquivo = uploadFile.getContents();
+		int lastIndexOf = uploadFile.getFileName().lastIndexOf('.');
+		
+		String colorReal = "XXXXXX".equals(color) ? colorPersonalizar : color;
+		
+		FileInput fileInput = new FileInput(uploadFile.getFileName().substring(0, lastIndexOf), colorReal);
+		
+		String arqString = new String(arquivo);
+		arqString = arqString.replace("\r\n", "");
+		arqString = arqString.replace(" ", "");
+		for(String instrucao : arqString.split(";")){
+			Word word = new Word(instrucao);
+			for (int i = 0; i < Constantes.WORD_SIZE; i++) {
+				fileInput.writeBytes(word.getIoWord()[i]);
+			}
+		}
+		return fileInput;
+	}
+	
+	private void clearUpload() {
+		uploadFile = null;
+		color = null;
+	}
+	
+	private boolean validarUploadArquivo(){
+		String message = "";
+		if(uploadFile == null || Util.isNullOuVazio(uploadFile.getFileName())) {
+			message += "É necessário selecionar um arquivo! <br/>";
+		} 
+		if (color == null || color.equals("")
+				|| (color.equals("XXXXXX") && (colorPersonalizar == null || colorPersonalizar.equals("")))) {
+			message += "É necessário selecionar uma cor!";
+		}
+		if (!Util.isNullOuVazio(message)) {
+			facesMessager.addMessageError(message);
+		}
+		return Util.isNullOuVazio(message);
+	}
+    
+	//ACCESS METHODS
+	public IDiskScheduler getDiskSchedule() {
+		return diskSchedule;
+	}
+
+	public void setDiskSchedule(IDiskScheduler diskSchedule) {
+		this.diskSchedule = diskSchedule;
+	}
+
+	public IDiskScheduler[] getArrayDiskSchedule() {
+		return arrayDiskSchedule;
+	}
+
+	public void setArrayDiskSchedule(IDiskScheduler[] arrayDiskSchedule) {
+		this.arrayDiskSchedule = arrayDiskSchedule;
+	}
+
+	public UploadedFile getUploadFile() {
+		return uploadFile;
+	}
+
+	public void setUploadFile(UploadedFile uploadFile) {
+		this.uploadFile = uploadFile;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String cor) {
+		this.color = cor;
+	}
+
+	public String getColorPersonalizar() {
+		return colorPersonalizar;
+	}
+
+	public void setColorPersonalizar(String colorPersonalizar) {
+		this.colorPersonalizar = colorPersonalizar;
+	}
+
+	public String getMovimentacaoSimulada() {
+		return movimentacaoSimulada;
+	}
+
+	public void setMovimentacaoSimulada(String movimentacaoSimulada) {
+		this.movimentacaoSimulada = movimentacaoSimulada;
+	}
+
+    //PARTE GRAFICA
 	/**
 	 * Utilizado para obter o arquivo por meio do número inicial do setor
 	 * 
@@ -36,10 +189,10 @@ public class DiscoController implements Serializable {
 	 * @return File
 	 */
 	private File obterArquivo(int nSector){
-		int idFile = getMaquinaSessaoController().seekIdFilePerSector(nSector);
+		int idFile = getSistemaArquivo().seekIdFilePerSector(nSector);
 		File arquivo = null;
 		if(idFile != -1) {
-			arquivo = getMaquinaSessaoController().seekFilePerId(idFile);
+			arquivo = getSistemaArquivo().seekFilePerId(idFile);
 		}
 		
 		if (arquivo == null) {
@@ -115,8 +268,8 @@ public class DiscoController implements Serializable {
 	 * 
 	 * @return ISistemaArquivo
 	 */
-	public IFileSystem getMaquinaSessaoController() {
-		return maquinaSessaoController.getSistemaArquivo();
+	public IFileSystem getSistemaArquivo() {
+		return operatingSystem.getFileSystem();
 	}
 		
 	/**
--- trunk/SWSO/src/main/java/br/edu/ifba/swso/controller/MaquinaSessaoController.java
+++ trunk/SWSO/src/main/java/br/edu/ifba/swso/controller/MaquinaSessaoController.java
@@ -8,22 +8,10 @@ import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.primefaces.model.UploadedFile;
-
-import br.edu.ifba.swso.algorithms.impl.disk.FCFS;
-import br.edu.ifba.swso.algorithms.impl.disk.SSTF;
-import br.edu.ifba.swso.algorithms.impl.process.FIFO;
-import br.edu.ifba.swso.algorithms.interfaces.IDiskScheduler;
-import br.edu.ifba.swso.algorithms.interfaces.IProcessesScheduler;
 import br.edu.ifba.swso.business.VirtualMachineParameters;
-import br.edu.ifba.swso.business.abstractions.File;
-import br.edu.ifba.swso.business.abstractions.FileInput;
-import br.edu.ifba.swso.business.abstractions.Word;
 import br.edu.ifba.swso.business.so.OperatingSystem;
-import br.edu.ifba.swso.business.so.filemanager.IFileSystem;
-import br.edu.ifba.swso.business.so.memorymanager.PageTable;
 import br.edu.ifba.swso.business.virtualmachine.CoreVirtualMachine;
-import br.edu.ifba.swso.util.Constantes;
+import br.edu.ifba.swso.display.TimelineDisplay;
 import br.edu.ifba.swso.util.Util;
 
 /**
@@ -38,46 +26,23 @@ public class MaquinaSessaoController extends BaseController implements Serializa
 	@Inject
 	private ApplicationController applicationController;
 	
-	//BUSSINESS - START
+	//BUSINESS - START
 	private CoreVirtualMachine coreVirtualMachine;
 	
 	private OperatingSystem operatingSystem;
 	
 	private VirtualMachineParameters virtualMachineParameters;
-	//BUSSINESS - END
 	
-	//DATE OF VIEW - START
-	private UploadedFile uploadFile;
+	private TimelineDisplay timelineDisplay;
 	
-	private String color;
-	
-	private String colorPersonalizar;
-	
-	private String movimentacaoSimulada;
-	
+	//DATA OF VIEW - START
 	private int activeAba;
 	
-	private IDiskScheduler diskSchedule;
-	
-	private IProcessesScheduler processesScheduler;
-	
-	private File file;
-	
-	private PageTable pageTable;
-	
-	private IDiskScheduler[] arrayDiskSchedule;
-	
-	private IProcessesScheduler[] arrayProcessesScheduler;
-	
-	private int timeslice = 5;
-	// DATE OF VIEW - END
-
+	//BUSINESS METHODS
 	@PostConstruct
 	public void init() {
 		virtualMachineParameters = new VirtualMachineParameters();
-		arrayDiskSchedule = new IDiskScheduler[]{new SSTF(virtualMachineParameters), new FCFS(virtualMachineParameters)};
-		arrayProcessesScheduler = new IProcessesScheduler[]{new FIFO()};
-		
+		timelineDisplay = new TimelineDisplay();
 	}
 	
 	@PreDestroy
@@ -87,17 +52,9 @@ public class MaquinaSessaoController extends BaseController implements Serializa
 	
 	public String initSimulation() {
 		if(validarName()) {
-			diskSchedule = arrayDiskSchedule[0];
-			processesScheduler = arrayProcessesScheduler[0];
 			coreVirtualMachine = new CoreVirtualMachine(virtualMachineParameters);
-			
 			operatingSystem = new OperatingSystem(coreVirtualMachine);
-			operatingSystem.setDiskSchedule(diskSchedule);
-			operatingSystem.setProcessesScheduler(processesScheduler);
-			operatingSystem.setTimeslice(timeslice);
-			
 			applicationController.getMaquinasAtivas().put(virtualMachineParameters.getName(), this);
-			
 			return includeRedirect("/paginas/simulacao/simulacao");
 		}
 		
@@ -107,8 +64,6 @@ public class MaquinaSessaoController extends BaseController implements Serializa
 	public String searchSimulation() {
 		if(searchMachine()) {
 			MaquinaSessaoController maquina = applicationController.getMaquinasAtivas().get(virtualMachineParameters.getName());
-			diskSchedule = maquina.getDiskSchedule();
-			processesScheduler = maquina.getProcessesScheduler();
 			coreVirtualMachine = maquina.getCoreVirtualMachine();
 			operatingSystem = maquina.getOperatingSystem();
 			return includeRedirect("/paginas/simulacao/simulacao-view-aluno");
@@ -120,79 +75,9 @@ public class MaquinaSessaoController extends BaseController implements Serializa
 	public void executarCiclo() {
 		operatingSystem.execute();
 		coreVirtualMachine.getCentralProcessingUnit().execute();
+		timelineDisplay.incrementList(operatingSystem.getProcessManager().getEmExecucao());
 	}
 	
-    public void doUploadFile() {
-    	if (validarUploadArquivo()) {
-    		FileInput fileInput = criarInputFile();
-    		operatingSystem.getFileSystem().allocateFile(fileInput, getOperatingSystem().getDiskSchedule());
-    		clearUpload();
-    		updateComponentes(":formSimulacao");
-    	}
-    }
-    
-    public void newProcess() {
-    	operatingSystem.criarProcesso(file);
-    	file = null;
-    	updateComponentes(":formSimulacao");
-    }
-    
-    public void salvarConfiguracoes() {
-    	operatingSystem.setDiskSchedule(diskSchedule);
-    }
-    
-    public void salvarConfiguracoesProcesso() {
-    	operatingSystem.setProcessesScheduler(processesScheduler);
-    	operatingSystem.setTimeslice(timeslice);
-    }
-    
-    public void deleteFile(Integer id) {
-    	operatingSystem.getFileSystem().deallocateFile(id);
-    }
-    
-	private FileInput criarInputFile() {
-		byte[] arquivo = uploadFile.getContents();
-		int lastIndexOf = uploadFile.getFileName().lastIndexOf('.');
-		
-		String colorReal = "XXXXXX".equals(color) ? colorPersonalizar : color;
-		
-		FileInput fileInput = new FileInput(uploadFile.getFileName().substring(0, lastIndexOf), colorReal);
-		
-		String arqString = new String(arquivo);
-		arqString = arqString.replace("\r\n", "");
-		arqString = arqString.replace(" ", "");
-		for(String instrucao : arqString.split(";")){
-			Word word = new Word(instrucao);
-			for (int i = 0; i < Constantes.WORD_SIZE; i++) {
-				fileInput.writeBytes(word.getIoWord()[i]);
-			}
-		}
-		return fileInput;
-	}
-
-	public void simularMovimentacao() {
-		operatingSystem.getFileSystem().simularMovimentacao(movimentacaoSimulada, getOperatingSystem().getDiskSchedule());
-	}
-	
-	private void clearUpload() {
-		uploadFile = null;
-		color = null;
-	}
-	
-	private boolean validarUploadArquivo(){
-		String message = "";
-		if(uploadFile == null || Util.isNullOuVazio(uploadFile.getFileName())) {
-			message += "É necessário selecionar um arquivo! <br/>";
-		} 
-		if (color == null || color.equals("")
-				|| (color.equals("XXXXXX") && (colorPersonalizar == null || colorPersonalizar.equals("")))) {
-			message += "É necessário selecionar uma cor!";
-		}
-		if (!Util.isNullOuVazio(message)) {
-			facesMessager.addMessageError(message);
-		}
-		return Util.isNullOuVazio(message);
-	}
 	
 	private boolean validarName() {
 		String message = "";
@@ -218,25 +103,9 @@ public class MaquinaSessaoController extends BaseController implements Serializa
 		return true;
 	}
 	
-	public void updateComboColor() {
-		updateComponentes(":uploadForm:selectColors");
-	}
-	
-	public void abrirModalPbc(int pid) {
-		  this.pageTable = operatingSystem.getMemoryManager().getPageList().get(pid);
-		  updateComponentes("modalPbcForm");
-		  showDialog("modalPbc");
-		  
-	}
-	
-
 	//MÉT0D0S DE ACESSO
 	public CoreVirtualMachine getCoreVirtualMachine() {
 		return coreVirtualMachine;
-	}
-	
-	public IFileSystem getSistemaArquivo() {
-		return operatingSystem.getFileSystem();
 	}
 	
 	public VirtualMachineParameters getVirtualMachineParameters() {
@@ -255,14 +124,6 @@ public class MaquinaSessaoController extends BaseController implements Serializa
 		this.virtualMachineParameters.setName(name);
 	}
 
-	public UploadedFile getUploadFile() {
-		return uploadFile;
-	}
-
-	public void setUploadFile(UploadedFile uploadFile) {
-		this.uploadFile = uploadFile;
-	}
-
 	public int getActiveAba() {
 		return activeAba;
 	}
@@ -270,89 +131,13 @@ public class MaquinaSessaoController extends BaseController implements Serializa
 	public void setActiveAba(int activeAba) {
 		this.activeAba = activeAba;
 	}
-	
-	public IDiskScheduler getDiskSchedule() {
-		return diskSchedule;
-	}
-
-	public void setDiskSchedule(IDiskScheduler diskSchedule) {
-		this.diskSchedule = diskSchedule;
-	}
-
-	public IDiskScheduler[] getArrayDiskSchedule() {
-		return arrayDiskSchedule;
-	}
-
-	public void setArrayDiskSchedule(IDiskScheduler[] arrayDiskSchedule) {
-		this.arrayDiskSchedule = arrayDiskSchedule;
-	}
-
-	public IProcessesScheduler getProcessesScheduler() {
-		return processesScheduler;
-	}
-
-	public void setProcessesScheduler(IProcessesScheduler processesScheduler) {
-		this.processesScheduler = processesScheduler;
-	}
-	
-	public IProcessesScheduler[] getArrayProcessesScheduler() {
-		return arrayProcessesScheduler;
-	}
-
-	public void setArrayProcessesScheduler(IProcessesScheduler[] arrayProcessesScheduler) {
-		this.arrayProcessesScheduler = arrayProcessesScheduler;
-	}
-
-	public int getTimeslice() {
-		return timeslice;
-	}
-
-	public void setTimeslice(int timeslice) {
-		this.timeslice = timeslice;
-	}
-
-	public String getColor() {
-		return color;
-	}
-
-	public void setColor(String cor) {
-		this.color = cor;
-	}
-
-	public String getColorPersonalizar() {
-		return colorPersonalizar;
-	}
-
-	public void setColorPersonalizar(String colorPersonalizar) {
-		this.colorPersonalizar = colorPersonalizar;
-	}
-
-	public String getMovimentacaoSimulada() {
-		return movimentacaoSimulada;
-	}
-
-	public void setMovimentacaoSimulada(String movimentacaoSimulada) {
-		this.movimentacaoSimulada = movimentacaoSimulada;
-	}
 
 	public OperatingSystem getOperatingSystem() {
 		return operatingSystem;
 	}
 
-	public File getFile() {
-		return file;
-	}
-
-	public void setFile(File file) {
-		this.file = file;
-	}
-
-	public PageTable getPageTable() {
-		return pageTable;
-	}
-
-	public void setPageTable(PageTable pageTable) {
-		this.pageTable = pageTable;
+	public TimelineDisplay getTimelineDisplay() {
+		return timelineDisplay;
 	}
 
 }
