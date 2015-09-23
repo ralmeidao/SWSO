208044d40c654bb5a53a7a2f2f6ccff25101cfb9
Ramon <Ramon@9009fd32-00b8-d34d-9bb3-231bff2beaf2>
2015-09-23 01:46:30 +0000

    Exibição da Memória Ram

---
+++ src/main/java/br/edu/ifba/swso/display/WordsDisplay.java
@@ -0,0 +1,128 @@
+package br.edu.ifba.swso.display;
+
+import br.edu.ifba.swso.business.abstractions.ByteSWSO;
+
+
+public class WordsDisplay {
+	private long positionColumn01;
+	private ByteSWSO valorColumn01;
+	
+	private long positionColumn02;
+	private ByteSWSO valorColumn02;
+	
+	private long positionColumn03;
+	private ByteSWSO valorColumn03;
+	
+	private long positionColumn04;
+	private ByteSWSO valorColumn04;
+	
+	private long positionColumn05;
+	private ByteSWSO valorColumn05;
+	
+	private long positionColumn06;
+	private ByteSWSO valorColumn06;
+	
+	private long positionColumn07;
+	private ByteSWSO valorColumn07;
+	
+	private long positionColumn08;
+	private ByteSWSO valorColumn08;
+	
+	public long getPositionColumn01() {
+		return positionColumn01;
+	}
+	public void setPositionColumn01(long positionColumn01) {
+		this.positionColumn01 = positionColumn01;
+	}
+	public ByteSWSO getValorColumn01() {
+		return valorColumn01;
+	}
+	public void setValorColumn01(ByteSWSO valorColumn01) {
+		this.valorColumn01 = valorColumn01;
+	}
+	public long getPositionColumn02() {
+		return positionColumn02;
+	}
+	public void setPositionColumn02(long positionColumn02) {
+		this.positionColumn02 = positionColumn02;
+	}
+	public ByteSWSO getValorColumn02() {
+		return valorColumn02;
+	}
+	public void setValorColumn02(ByteSWSO valorColumn02) {
+		this.valorColumn02 = valorColumn02;
+	}
+	public long getPositionColumn03() {
+		return positionColumn03;
+	}
+	public void setPositionColumn03(long positionColumn03) {
+		this.positionColumn03 = positionColumn03;
+	}
+	public ByteSWSO getValorColumn03() {
+		return valorColumn03;
+	}
+	public void setValorColumn03(ByteSWSO valorColumn03) {
+		this.valorColumn03 = valorColumn03;
+	}
+	public long getPositionColumn04() {
+		return positionColumn04;
+	}
+	public void setPositionColumn04(long positionColumn04) {
+		this.positionColumn04 = positionColumn04;
+	}
+	public ByteSWSO getValorColumn04() {
+		return valorColumn04;
+	}
+	public void setValorColumn04(ByteSWSO valorColumn04) {
+		this.valorColumn04 = valorColumn04;
+	}
+	public long getPositionColumn05() {
+		return positionColumn05;
+	}
+	public void setPositionColumn05(long positionColumn05) {
+		this.positionColumn05 = positionColumn05;
+	}
+	public ByteSWSO getValorColumn05() {
+		return valorColumn05;
+	}
+	public void setValorColumn05(ByteSWSO valorColumn05) {
+		this.valorColumn05 = valorColumn05;
+	}
+	public long getPositionColumn06() {
+		return positionColumn06;
+	}
+	public void setPositionColumn06(long positionColumn06) {
+		this.positionColumn06 = positionColumn06;
+	}
+	public ByteSWSO getValorColumn06() {
+		return valorColumn06;
+	}
+	public void setValorColumn06(ByteSWSO valorColumn06) {
+		this.valorColumn06 = valorColumn06;
+	}
+	public long getPositionColumn07() {
+		return positionColumn07;
+	}
+	public void setPositionColumn07(long positionColumn07) {
+		this.positionColumn07 = positionColumn07;
+	}
+	public ByteSWSO getValorColumn07() {
+		return valorColumn07;
+	}
+	public void setValorColumn07(ByteSWSO valorColumn07) {
+		this.valorColumn07 = valorColumn07;
+	}
+	public long getPositionColumn08() {
+		return positionColumn08;
+	}
+	public void setPositionColumn08(long positionColumn08) {
+		this.positionColumn08 = positionColumn08;
+	}
+	public ByteSWSO getValorColumn08() {
+		return valorColumn08;
+	}
+	public void setValorColumn08(ByteSWSO valorColumn08) {
+		this.valorColumn08 = valorColumn08;
+	}
+	
+}
