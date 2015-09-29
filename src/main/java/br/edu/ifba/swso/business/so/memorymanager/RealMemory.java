39adabaa9a4ba4d90c8bbad8b86d73877934fe8d
Ramon <Ramon@9009fd32-00b8-d34d-9bb3-231bff2beaf2>
2015-09-29 00:55:28 +0000

    Simulação de Memoria

--- src/main/java/br/edu/ifba/swso/business/so/memorymanager/RealMemory.java
+++ src/main/java/br/edu/ifba/swso/business/so/memorymanager/RealMemory.java
@@ -1,16 +1,22 @@
 package br.edu.ifba.swso.business.so.memorymanager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.edu.ifba.swso.business.so.memorymanager.exception.MemoryFullException;
 import br.edu.ifba.swso.util.Constantes;
 
 public class RealMemory {
 	
 	private char[] realMemory;
+	private int[] processAlloc;
 	
 	public RealMemory() {
 		realMemory = new char[Constantes.MEMORY_SIZE*Constantes.BYTE_PER_PAGE];
+		processAlloc = new int[Constantes.MEMORY_SIZE*Constantes.BYTE_PER_PAGE];
 		for (int i = 0; i < realMemory.length; i++) {
 			realMemory[i] = '0';
+			processAlloc[i] = -1;
 		}
 	}
 
@@ -27,13 +33,18 @@ public class RealMemory {
 		realMemory[pos] = '0';
 	}
 	
-	public void blockPosition(int pos) {
+	public void blockPosition(int pos, int pid) {
 		realMemory[pos] = '1';
+		processAlloc[pos] = pid;
 	}
 
 	//MÉTHODS OF ACCESS
-	public char[] getVirtualMemory() {
-		return realMemory;
+	public List<Integer> getRealMemory() {
+		List<Integer> lista = new ArrayList<Integer>();
+		for (int i = 0; i < processAlloc.length; i++) {
+			lista.add(processAlloc[i]);
+		}
+		return lista;
 	}
 
 }
